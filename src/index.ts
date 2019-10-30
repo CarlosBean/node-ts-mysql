78f58f32df16435bd22d34ca97a393463d5f68a8
Carlos Benavides <1992cab@gmail.com>
2019-10-30 15:16:03 -0500

    chore: fix bad declarations on index

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,13 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 
 const server = Server.init();
-server.app.use(router);
 
 server.app.use(bodyParser.urlencoded({ extended: true }));
 server.app.use(bodyParser.json());
 
 server.app.use(cors({ origin: true, credentials: true }));
+
+server.app.use(router);
 
 server.start(() => {
     console.log(`Server running on port: ${server.port}`);
